73f1fdccd2389284dc9863c64bbaeaf184cae123
siyajari samuel <siyajari.samuel@student.moringaschool.com>
2024-05-23 08:52:02 +0300

    Control flow loops

--- lib/looping.py
+++ lib/looping.py
@@ -1,13 +1,29 @@
 #!/usr/bin/env python3
 
 def happy_new_year():
-    # code goes here!
-    pass
+    counter = 10
+    while counter > 0:
+        print(counter)
+        counter -= 1
+    print("Happy New Year!")
 
 def square_integers(int_list):
-    # code goes here!
-    pass
+    return list(map(lambda num: num ** 2, int_list))
+    print(square_integers([1, 2, 3, 4]))
 
-def fizzbuzz():
-    # code goes here!
-    pass
+
+def fizzbuzz(num,):
+    if num % 3 == 0 and num % 5 == 0:
+        return "FizzBuzz"
+    elif num % 3 == 0:
+        return "Fizz"
+    elif num % 5 == 0:
+        return "Buzz"
+    else:
+        return num
+    
+
+
+def fizzbuzz_printer():
+    for num in range(1, 101):
+        print(fizzbuzz(num))
